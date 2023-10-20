dd1ddb8ff1c6e787b3ee3624ed4803e982bb61b4
MarijaSkuja <marija.skuja@grobinasskola.lv>
2023-10-20 10:19:46 +0300

    skaitli

--- sniegs.py
+++ sniegs.py
@@ -1,1 +1,6 @@
-print("Labs rīts!")
+import random
+# Ģenerē 100 skaitļus un izvada tos terminālī
+skaitlu_saraksts = [random.randint(1, 100) for _ in range(100)]
+# izvada skaitļus terminālī
+print("100 ģenerētie skaitļi")
+print(skaitlu_saraksts)
